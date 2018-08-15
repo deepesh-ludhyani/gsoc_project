51f2e5d0b8c0ffd3e400630ac19a24fcb3567381
deepesh ludhyani <deepeshludhiyani@gmail.com>
2018-08-15 19:33:22 +0530

    working of the calculator upgraded via js file

--- Calculator/working.js
+++ Calculator/working.js
@@ -3,4 +3,34 @@ window.onload = function()
 	var buttons = document.getElementById('Buttons');
 	var clear = document.getElementById('clear');
 	var answer = document.getElementById('answer');
-}
+
+	Buttons.addEventListener('click', function(e)
+		{
+			
+			if(e.target.nodeName === 'LI')
+			{
+				var v = e.target.innerHTML;
+				if(v === '=')
+				{
+					//evaluating here
+					try{
+					answer.innerHTML = eval(answer.innerHTML);
+				}
+				catch(e)
+				{
+					answer.innerHTML = e.message;
+				}
+			}
+				else{
+				answer.innerHTML+=v;
+
+		}		//console.log('button clicked!');
+			}
+			//e.target.style.display = 'none';
+		});
+
+	  // clear the answer field
+  clear.addEventListener('click', function() {
+    answer.innerHTML = '';
+  });
+};
